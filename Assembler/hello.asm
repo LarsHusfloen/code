f5e0f93943001374831facf259045da41036d3f5
Lars <larsbh@uia.no>
2021-03-10 14:28:49 +0100

    Endret hello filen

--- Assembler/hello.asm
+++ Assembler/hello.asm
@@ -1,26 +1,14 @@
-section .text                   ;section declaration
-
-                                ;we must export the entry point to the ELF linker or
-    global  _start              ;loader. They conventionally recognize _start as their
-			                          ;entry point. Use ld -e foo to override the default.
-
-_start:
-
-                                ;write our string to stdout
-
-    mov     edx,len             ;third argument: message length
-    mov     ecx,msg             ;second argument: pointer to message to write
-    mov     ebx,1               ;first argument: file handle (stdout)
-    mov     eax,4               ;system call number (sys_write)
-    int     0x80                ;call kernel
-
-                                ;and exit
-
-  	mov     ebx,0               ;first syscall argument: exit code
-    mov     eax,1               ;system call number (sys_exit)
-    int     0x80                ;call kernel
-
-section .data                   ;section declaration
-
-msg db      "Hello, world!",0xa ;our dear string
-len equ     $ - msg             ;length of our dear string
+global start
+section .text
+start:
+  mov rax, 0x2000004 ; write
+  mov rdi, 1 ; stdout
+  mov rsi, msg
+  mov rdx, msg.len
+  syscall
+  mov rax, 0x2000001 ; exit
+  mov rdi, 0
+  syscall
+section .data
+msg:    db      "Hello, world!", 10
+.len:   equ     $ - msg
