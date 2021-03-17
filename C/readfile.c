dec08874668edd034f3f65f3df46135bb931c5f7
Lars <larsbh@uia.no>
2021-03-17 16:15:49 +0100

    Fikset readfile så den tar inn filen som skal leses

--- C/readfile.c
+++ C/readfile.c
@@ -1,20 +1,21 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(){
 int fd, sz;
 char *buf = (char * ) calloc(20, sizeof(char));
-char fileName;
+char fileName[100];
 
 printf("Enter the file to scan: ");
-scanf("%c",&fileName);
+scanf("%s", fileName);
 fd = open(fileName, O_RDONLY);
 if (fd < 0) {
 	perror("feil ves lesning av fil"); exit(1);
 }
-sz = read(fd, buf, 10);
-printf("Gjorde et kall til read(%d, buf, 10). Returnerte at %d bytes ble lest.\n", fd, sz);
+sz = read(fd, buf, 20);
+printf("Gjorde et kall til read(%d, buf, 20). Returnerte at %d bytes ble lest.\n", fd, sz);
 printf("Bytes som ble lest var: %s\n", buf);
 close(fd);
 }
-
